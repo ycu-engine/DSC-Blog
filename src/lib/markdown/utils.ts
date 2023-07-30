482129aa1abb53007163d7af9355114ff9991310
N_ha <69199126+N-ha-git@users.noreply.github.com>
2023-07-30 18:38:56 +0900

    ✨ remark の導入

---
+++ src/lib/markdown/utils.ts
@@ -0,0 +1,18 @@
+import { remark } from 'remark'
+import remarkRehype from 'remark-rehype'
+import rehypeReact from 'rehype-react'
+import React from 'react'
+
+/**
+ * markdown to react JSX
+ * @param markdown markdown
+ * @returns react JSX
+ */
+export const mark2react = async (markdown: string) => {
+  const result = await remark()
+    .use(remarkRehype)
+    .use(rehypeReact, { createElement: React.createElement, Fragment: React.Fragment })
+    .process(markdown)
+  const react = result.result
+  return react
+}
