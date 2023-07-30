69e3104efa49945fe269327cc5dab23e1cfa8da7
nyatinte <takumi.062757@gmail.com>
2023-07-30 22:40:22 +0900

    ✨ホーム画面を仮で作成

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['source.unsplash.com'],
+  },
 }
 
 module.exports = nextConfig
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,97 @@
-import { NextPage } from 'next'
-import { Inter } from 'next/font/google'
+import { GetStaticProps, NextPage } from 'next'
 import Image from 'next/image'
+import Link from 'next/link'
+import { Heading } from '@/components/ui/heading'
+import { Post } from '@/types/Post'
 
-const inter = Inter({ subsets: ['latin'] })
+type PageProps = {
+  posts: Post[]
+}
 
 /**
  * ホーム画面
  */
-const Home: NextPage = () => (
-  <main
-    className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-  >
-    <div className="before:bg-gradient-radial after:bg-gradient-conic relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-      <Image src="/logo.png" alt="データサイエンス倶楽部 ロゴ" width={1000} height={600} priority />
+const Home: NextPage<PageProps> = ({ posts }) => (
+  <main className="flex min-h-screen flex-col gap-16 p-8">
+    <Heading variant="title" className="italic">
+      New Posts
+    </Heading>
+    <div className="flex gap-4">
+      {posts.map((post) => (
+        <Link href={`/posts/${post.id}`} key={post.id} className="duration-200 hover:opacity-90">
+          <div key={post.id} className="flex flex-col gap-4">
+            <Image
+              src={post.coverImageUri}
+              alt="Cover Image"
+              width={800}
+              height={600}
+              className="rounded-lg"
+            />
+            <p className="text-lg">・{post.author.name}</p>
+            <p className="text-2xl font-bold">{post.title}</p>
+          </div>
+        </Link>
+      ))}
     </div>
   </main>
 )
+
+/**
+ * SSG 記事の内容を取得する
+ */
+export const getStaticProps: GetStaticProps<PageProps> = async () => {
+  const samplePosts: Post[] = [
+    {
+      id: '1',
+      author: {
+        id: '1',
+        name: 'テスト花子',
+        email: 'testHanako@ycu.com',
+      },
+      title:
+        'Cupidatat est veniam ad ut ullamco tempor. Irure et deserunt proident Lorem dolore reprehenderit enim esse cupidatat sit Lorem nisi nostrud Lorem ad.',
+      content: '記事の内容',
+      createdAt: '2021-01-01T00:00:00.000Z',
+      updatedAt: '2021-01-01T00:00:00.000Z',
+      coverImageUri: 'https://source.unsplash.com/random/800x800',
+      isPublic: true,
+    },
+    {
+      id: '2',
+      author: {
+        id: '2',
+        name: 'テスト太郎',
+        email: 'testTarou@ycu.com',
+      },
+      title:
+        'Cupidatat est veniam ad ut ullamco tempor. Irure et deserunt proident Lorem dolore reprehenderit enim esse cupidatat sit Lorem nisi nostrud Lorem ad.',
+      content: '記事の内容',
+      createdAt: '2021-01-01T00:00:00.000Z',
+      updatedAt: '2021-01-01T00:00:00.000Z',
+      coverImageUri: 'https://source.unsplash.com/random/800x800',
+      isPublic: true,
+    },
+    {
+      id: '3',
+      author: {
+        id: '3',
+        name: 'テスト三郎',
+        email: 'ds@ycu.com',
+      },
+      title:
+        'Cupidatat est veniam ad ut ullamco tempor. Irure et deserunt proident Lorem dolore reprehenderit enim esse cupidatat sit Lorem nisi nostrud Lorem ad.',
+      content: '記事の内容',
+      createdAt: '2021-01-01T00:00:00.000Z',
+      updatedAt: '2021-01-01T00:00:00.000Z',
+      coverImageUri: 'https://source.unsplash.com/random/800x800',
+      isPublic: true,
+    },
+  ]
+  return {
+    props: {
+      posts: samplePosts,
+    },
+  }
+}
 
 export default Home
