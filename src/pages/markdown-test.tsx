af9d7bfacb56b315144839233a2ccb7d4c74c8d3
N_ha <69199126+N-ha-git@users.noreply.github.com>
2023-08-02 12:20:45 +0900

    🎨 event handler の inline関数 を別で定義

--- src/pages/markdown-test.tsx
+++ src/pages/markdown-test.tsx
@@ -8,16 +8,15 @@ import { markHtml, markPlainText } from '@/lib/markdown/utils'
  */
 const MarkdownTest: NextPage = () => {
   const [markdown, setMarkdown] = React.useState<string>('')
+  const handleChangeTextarea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMarkdown(e.target.value)
+  }
   return (
     <div className="m-4 flex flex-col gap-2">
       <h1 className="mb-4 self-center">Markdown のテストページ</h1>
       <div className="mb-2">
         <h2>入力</h2>
-        <textarea
-          className="w-full border-2"
-          onChange={(e) => setMarkdown(e.target.value)}
-          value={markdown}
-        />
+        <textarea className="w-full border-2" onChange={handleChangeTextarea} value={markdown} />
       </div>
       <div className="mb-2">
         <h2>Markdown</h2>
